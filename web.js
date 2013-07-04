760fba2280fc43342e26edf9b00cfac582150e04
Mikhail Pokidko <pma@altlinux.org>
2013-07-04 16:40:42 +0400

    fix typo

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var fs = require('fs');
 
-var index = fs.readFileSync('index.html')toString("utf-8");
+var index = fs.readFileSync('index.html').toString("utf-8");
 
 var app = express.createServer(express.logger());
 
